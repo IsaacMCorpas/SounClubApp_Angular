42acc59ad3b903ccfbb62d5edef90437fdd6f45f
YoloKazy <carlesgarro94@gmail.com>
2018-07-19 19:55:21 +0200

    editarperfil html estructura echa

--- src/app/editar-perfil/editar-perfil.component.ts
+++ src/app/editar-perfil/editar-perfil.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../modelos/usuario';
+import { HttpClient } from '../../../node_modules/@angular/common/http';
+import { Router } from '../../../node_modules/@angular/router';
+import { Pais } from '../modelos/pais';
+import { UsuarioService } from '../services/usuario.service';
+import { NgForm } from '../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'app-editar-perfil',
@@ -6,10 +12,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-perfil.component.css']
 })
 export class EditarPerfilComponent implements OnInit {
+  usuario:Usuario = new Usuario(0, "Isaac", "Mesa", "is@is.com", "1234", "");
+  unPais: Pais = new Pais (0, "AFG", "Afganistan");
 
-  constructor() { }
+  constructor(private _usuarioService:UsuarioService, private _httpClient: HttpClient, private _router:Router) { }
 
   ngOnInit() {
+    // this._usuarioService.getUsuarioById(this.usuario.uid).subscribe(losUsuarios=>
+    //   {this.usuario=losUsuarios;}
+    // );
   }
-
+  onSubmit(editForm:NgForm){
+    if(editForm.valid){console.log(this.usuario)
+      
+    }
+  }
 }
