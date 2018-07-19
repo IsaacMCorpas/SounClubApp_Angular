5869b6e980edf47570718f14e6bbe2d84b485dd3
YoloKazy <carlesgarro94@gmail.com>
2018-07-19 17:03:14 +0200

    App Routing Module Updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './commons/page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
+import {EditarPerfilComponent} from './editar-perfil/editar-perfil.component';
+import {RegistroComponent} from './registro/registro.component';
+import {ListaAmigoComponent} from './lista-amigo/lista-amigo.component'
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'editarperfil', component: EditarPerfilComponent, pathMatch: 'full' },
+  { path: 'registro', component: RegistroComponent, pathMatch: 'full' },
+  { path: 'listaamigos', component: ListaAmigoComponent, pathMatch: 'full' },
+  { path: '',redirectTo: '/login',pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent }
   
 ];
 
