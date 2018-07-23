89dfdf05ff1026a8a1ae1d22fb82e944039884e7
Pedro <360k.cat@gmail.com>
2018-07-23 20:06:28 +0200

    subir

--- src/app/lista-usuario-buscado/lista-usuario-buscado.component.ts
+++ src/app/lista-usuario-buscado/lista-usuario-buscado.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../modelos/usuario';
+import { UsuarioService } from '../services/usuario.service';
+import { AuthService } from '../services/auth.service';
+import { Route } from '@angular/compiler/src/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-lista-usuario-buscado',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-usuario-buscado.component.css']
 })
 export class ListaUsuarioBuscadoComponent implements OnInit {
+  listaUsuarios:Usuario[];
+  nombreABuscar:string;
 
-  constructor() { }
+
+
+  constructor(private _usuarioService:UsuarioService, private _authService:AuthService, private _route:ActivatedRoute) { }
 
   ngOnInit() {
-  }
 
+    this._route.params.subscribe(pathparams=>{
+      this.nombreABuscar=pathparams['nombre'];
+    });
+
+    this._usuarioService.getUsuariosBuscados(this.nombreABuscar).subscribe(lista => {
+        this.listaUsuarios = lista;
+    });
+
+    }
 }
+
+
